55828c7d019c8d35d19a2a6cae4e16bdf672ceb3
Soham Metha <sohammetha01@gmail.com>
2025-07-28 16:26:36 +0000

    28/07/2025 : 21:56:36

--- tests/hammingTests.c
+++ tests/hammingTests.c
@@ -20,11 +20,11 @@ int main(int argc, char *argv[])
 {
     assert(argc == 6);
     char senderCmd[256];
-    snprintf(senderCmd, sizeof(senderCmd), "%s %s %s >/dev/null", argv[PARAM_SENDER], argv[PARAM_INPUT_FILE],
+    snprintf(senderCmd, sizeof(senderCmd), "%s %s %s ", argv[PARAM_SENDER], argv[PARAM_INPUT_FILE],
              argv[PARAM_INTERMEDIATE_FILE]);
-    snprintf(receiverCmd, sizeof(receiverCmd), "%s %s %s >/dev/null", argv[PARAM_RECEIVER],
-             argv[PARAM_INTERMEDIATE_FILE], argv[PARAM_OUTPUT_FILE]);
-    snprintf(diffCmd, sizeof(diffCmd), "diff %s %s >/dev/null", argv[PARAM_INPUT_FILE], argv[PARAM_OUTPUT_FILE]);
+    snprintf(receiverCmd, sizeof(receiverCmd), "%s %s %s ", argv[PARAM_RECEIVER], argv[PARAM_INTERMEDIATE_FILE],
+             argv[PARAM_OUTPUT_FILE]);
+    snprintf(diffCmd, sizeof(diffCmd), "diff %s %s ", argv[PARAM_INPUT_FILE], argv[PARAM_OUTPUT_FILE]);
     printf("RUNNING SENDER   : %s\n\n", senderCmd);
     assert(system(senderCmd) == 0);
     printf("RUNNING RECEIVER : %s\n\n", receiverCmd);
