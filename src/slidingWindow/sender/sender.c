9271de06f54e1b5ff9f6844ca4408f2d27a8a4ce
Soham Metha <sohammetha01@gmail.com>
2025-07-28 16:57:40 +0000

    28/07/2025 : 22:27:40

--- src/slidingWindow/sender/sender.c
+++ src/slidingWindow/sender/sender.c
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
     {
         while ((sentCnt - ackCnt + 1) < WIN_SIZE)
         {
-            printf("\nsending %3d '%c'", sentCnt, (in[sentCnt] == '\n') ? '\\' : in[sentCnt]);
+            printf("\nsending %3d/%3d '%c'", sentCnt, fileSize, (in[sentCnt] == '\n') ? '\\' : in[sentCnt]);
             sentCnt += 1;
         }
         getAck();
