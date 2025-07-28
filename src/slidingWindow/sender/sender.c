728996caae41f04fb0c0cc740369c0047f512042
Soham Metha <sohammetha01@gmail.com>
2025-07-28 16:33:47 +0000

    28/07/2025 : 22:03:47

---
+++ src/slidingWindow/sender/sender.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("\n================= SENDER =================\n");
+    long fileSize = getFiles(&argc, &argv);
+
+    printf("\n┌─────┬──────────────┐\n");
+    for (long i = 0; i < fileSize; i++)
+    {
+        
+    }
+
+
+    printf("└─────┴──────────────┘"
+           "\n\n================= SENDER =================\n\n");
+    return 0;
+}
