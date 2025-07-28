ae1b4cc1cc12a14bd921d367aecf9f4f6a68c0af
Soham Metha <sohammetha01@gmail.com>
2025-07-28 15:08:38 +0000

    28/07/2025 : 20:38:38

--- src/ping/ping.c
+++ src/ping/ping.c
@@ -1,5 +1,18 @@
+#include <assert.h>
 #include <ping.h>
+#include <stdlib.h>
 
 icmp *icmpConstructor(int8 type, int8 code, int8 *data, int16 len)
 {
+    if (!data || !len)
+        return (icmp *)0;
+
+    icmp *pkt;
+    int16 pktSize = len + sizeof(struct icmp);
+    pkt = (icmp *)malloc($i pktSize);
+    assert(pkt);
+
+    pkt->code = code;
+    pkt->type = type;
+    pkt->checksum = getChecksum(pkt);
 }
