9eca2ce33dac0ef26dac8201e2b5ad46a331bac5
Soham Metha <sohammetha01@gmail.com>
2025-07-28 16:45:08 +0000

    28/07/2025 : 22:15:08

--- src/slidingWindow/sender/sender.c
+++ src/slidingWindow/sender/sender.c
@@ -1,5 +1,16 @@
 #include <GBN.h>
+#include <fileOps.h>
 #include <stdio.h>
+
+int ackCnt = 0, sentCnt = 0;
+void getAck()
+{
+    int ack;
+    printf("\nNo ACK: -1"
+           "\n   ACK: no.");
+    scanf("%d", &ack);
+    ackCnt = (ack == -1) ? ackCnt : ack;
+}
 
 int main(int argc, char **argv)
 {
@@ -7,8 +18,14 @@ int main(int argc, char **argv)
     long fileSize = getFiles(&argc, &argv);
 
     printf("\n┌─────┬──────────────┐\n");
-    for (long i = 0; i < fileSize; i++)
+    for (long i = 0; sentCnt < fileSize; i++)
     {
+        while ((sentCnt - ackCnt) < WIN_SIZE)
+        {
+            printf("sending %d %c", sentCnt, in[i]);
+        }
+        getAck();
+        sentCnt = ackCnt;
     }
 
     printf("└─────┴──────────────┘"
