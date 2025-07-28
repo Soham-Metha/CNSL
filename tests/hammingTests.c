822799ca44068f255b8af3dcd6b4bf761476ee65
Soham Metha <sohammetha01@gmail.com>
2025-07-28 14:36:37 +0000

    28/07/2025 : 20:06:37

---
+++ tests/hammingTests.c
@@ -0,0 +1,33 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum Params
+{
+    PARAM_FILE_NAME,
+    PARAM_SENDER,
+    PARAM_RECEIVER,
+    PARAM_INPUT_FILE,
+    PARAM_INTERMEDIATE_FILE,
+    PARAM_OUTPUT_FILE,
+} Params;
+
+char senderCmd[256];
+char receiverCmd[256];
+char diffCmd[256];
+
+int main(int argc, char *argv[])
+{
+    assert(argc == 5);
+    char senderCmd[256];
+    snprintf(senderCmd, sizeof(senderCmd), "%s %s %s", argv[PARAM_SENDER], argv[PARAM_INPUT_FILE],
+             argv[PARAM_INTERMEDIATE_FILE]);
+    snprintf(receiverCmd, sizeof(receiverCmd), "%s %s %s", argv[PARAM_RECEIVER], argv[PARAM_INTERMEDIATE_FILE],
+             argv[PARAM_OUTPUT_FILE]);
+    snprintf(diffCmd, sizeof(diffCmd), "diff %s %s", argv[PARAM_INPUT_FILE], argv[PARAM_OUTPUT_FILE]);
+    assert(system(senderCmd) == 0);
+    assert(system(receiverCmd) == 0);
+    assert(system(diffCmd) == 0);
+
+    return 0;
+}
