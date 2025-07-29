0857c9cf4bfaed11d28ea3e1fafc7cad6ae4a0bf
Soham Metha <sohammetha01@gmail.com>
2025-07-29 19:54:32 +0000

    30/07/2025 : 01:24:32

--- src/hamming/sender/hammingEncoder.c
+++ src/hamming/sender/hammingEncoder.c
@@ -6,7 +6,7 @@ void getPrePriorityChangeValue(char ch)
 {
     int i = 1;
 
-    for (char chLen = DATA_BIT_CNT - 1, powOf2 = 1; chLen >= 0; i++)
+    for (char chLen = DATA_BIT_CNT - 1, powOf2 = 1; chLen >= 0;i++)
     {
         if (i == powOf2)
         {
@@ -37,6 +37,7 @@ void getPrePriorityChangeValue(char ch)
         {
             chLen--;
         }
+        i++;
     }
 }
 
