1f65c91615999b502d4ec507a68b4940c166f52a
Soham Metha <sohammetha01@gmail.com>
2025-07-29 20:10:55 +0000

    30/07/2025 : 01:40:55

--- src/hamming/sender/hammingEncoder.c
+++ src/hamming/sender/hammingEncoder.c
@@ -2,8 +2,16 @@
 #include <stdio.h>
 unsigned char arr[MESSAGE_SIZE + 1];
 
-void getPrePriorityChangeValue(char ch)
+unsigned short getCodeFor(char ch)
 {
+    for (unsigned char i = 0; i <= MESSAGE_SIZE; i++)
+    {
+        arr[i] = 0;
+    }
+
+    getPrePriorityChangeValue(ch);
+    unsigned short result = 0;
+
     unsigned char i = 1;
     for (char chLen = DATA_BIT_CNT - 1, powOf2 = 1; chLen >= 0; i++)
     {
@@ -24,32 +32,6 @@ void getPrePriorityChangeValue(char ch)
 
         chLen--;
     }
-}
-
-unsigned short getCodeFor(char ch)
-{
-    for (unsigned char i = 0; i <= MESSAGE_SIZE; i++)
-    {
-        arr[i] = 0;
-    }
-
-    getPrePriorityChangeValue(ch);
-    unsigned short result = 0;
-
-    // for (unsigned char powOf2 = 1; powOf2 <= MESSAGE_SIZE; powOf2 = powOf2 << 1)
-    // {
-    //     char parity = 0;
-
-    //     for (int j = powOf2; j <= MESSAGE_SIZE; j = j + 2 * powOf2)
-    //         for (unsigned char k = j; k < j + powOf2 && k <= MESSAGE_SIZE; k++)
-    //         {
-    //             if (arr[k])
-    //             {
-    //                 parity = !parity;
-    //             }
-    //         }
-    //     arr[powOf2] = parity;
-    // }
     for (int i = 1; i <= MESSAGE_SIZE; i++)
     {
         result |= arr[i] << (MESSAGE_SIZE - i);
