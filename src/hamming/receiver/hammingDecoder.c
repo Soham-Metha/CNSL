d79e72032cf3f4b883a97d6d7241de4a0b2fa568
Soham Metha <sohammetha01@gmail.com>
2025-07-29 20:40:34 +0000

    30/07/2025 : 02:10:34

--- src/hamming/receiver/hammingDecoder.c
+++ src/hamming/receiver/hammingDecoder.c
@@ -41,7 +41,7 @@ char getDecodedCharFrom(unsigned short code)
     }
 
     unsigned char ParityVal = 0;
-    printf("%d %d %d %d", arr[1 << 0], arr[2], arr[4], arr[8]);
+    printf("%d %d %d %d", arr[1 << 0], arr[1 << 1], arr[1 << 2], arr[1 << 3]);
     // for (unsigned char j = 0; j < MESSAGE_SIZE; j = j << 1)
     //     ParityVal = j * arr[j];
 
