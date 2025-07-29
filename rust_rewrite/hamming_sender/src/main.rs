6405c6410856be8e345975764853c5212522f2a6
Soham Metha <sohammetha01@gmail.com>
2025-07-29 15:33:16 +0000

    29/07/2025 : 21:03:16

--- rust_rewrite/hamming_sender/src/main.rs
+++ rust_rewrite/hamming_sender/src/main.rs
@@ -1,5 +1,22 @@
+use std::env;
+
+fn get_files(args: Vec<String>) {
+    println!("All args: {:?}", args);
+
+    if args.len() < 3 {
+        eprintln!("Usage: {} <in_file_name> <in_file_name>", args[0]);
+        return;
+    }
+
+    let in_file_name = &args[1];
+    let out_file_name: &String = &args[2];
+    println!("\nIN : {}\nOUT: {}\n", in_file_name, out_file_name);
+}
+
 fn main() {
+    let args = env::args().collect();
     println!("\n================= SENDER =================\n");
+    get_files(args);
 
     println!("\n┌─────┬──────────────┐\n");
     println!("└─────┴──────────────┘");
