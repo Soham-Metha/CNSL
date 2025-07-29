730ca7e156bf433f5b70da7d16c00fce3578ce4c
Soham Metha <sohammetha01@gmail.com>
2025-07-29 15:38:51 +0000

    29/07/2025 : 21:08:51

--- rust_rewrite/hamming_sender/src/main.rs
+++ rust_rewrite/hamming_sender/src/main.rs
@@ -1,16 +1,17 @@
 use std::env;
+use std::fs;
 
 fn get_files(args: Vec<String>) {
-    println!("All args: {:?}", args);
+    // println!("All args: {:?}", args);
 
     if args.len() < 3 {
         eprintln!("Usage: {} <in_file_name> <in_file_name>", args[0]);
         return;
     }
 
-    let in_file_name = &args[1];
-    let out_file_name: &String = &args[2];
-    println!("\nIN : {}\nOUT: {}\n", in_file_name, out_file_name);
+    let in_file = fs::read(&args[1]).expect("CANT OPEN IN FILE");
+    let out_file = fs::read(&args[2]).expect("CANT OPEN OUT FILE");
+    println!("\nIN : {}\nOUT: {}\n", args[1], args[2]);
 }
 
 fn main() {
