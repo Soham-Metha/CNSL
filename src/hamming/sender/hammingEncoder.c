65476e4bab1abc4a08e6a84c7f0e6692c2488b5e
Soham Metha <sohammetha01@gmail.com>
2025-07-29 19:42:22 +0000

    30/07/2025 : 01:12:22

--- src/hamming/sender/hammingEncoder.c
+++ src/hamming/sender/hammingEncoder.c
@@ -2,24 +2,6 @@
 #include <stdio.h>
 unsigned char arr[MESSAGE_SIZE + 1];
 
-// void getMsbPosition(HammingSymbol *wrd)
-// {
-//     while (wrd->charValue >> wrd->dataBitCnt)
-//     {
-//         wrd->dataBitCnt++;
-//     }
-//     wrd->dataBitCnt = 8;
-// }
-
-// void getPriorityBitCnt(HammingSymbol *wrd)
-// {
-//     unsigned char bits = 1;
-//     while ((bits << wrd->parityBitCnt) < (wrd->dataBitCnt + 1 + wrd->parityBitCnt))
-//     {
-//         wrd->parityBitCnt++;
-//     }
-// }
-
 void getPrePriorityChangeValue(char ch)
 {
     int i = 1;
@@ -27,12 +9,34 @@ void getPrePriorityChangeValue(char ch)
     for (char chLen = DATA_BIT_CNT, powOf2 = 1; chLen > 0;)
     {
         if (i == powOf2)
+        {
             powOf2 = powOf2 << 1;
+        }
         else
         {
-            arr[i] = (ch & (1 << (chLen - 1))) ? 1 : 0;
+            // arr[i] = (ch & (1 << (chLen - 1))) ? 1 : 0;
             // printf("%d", arr[i]);
             chLen--;
+            if (ch & (1 << (chLen - 1)))
+            {
+                arr[i] = 1;
+                if (i & (1 << 0))
+                {
+                    arr[1 << 0] = !arr[1 << 0];
+                }
+                else if (i & (1 << 1))
+                {
+                    arr[1 << 1] = !arr[1 << 1];
+                }
+                else if (i & (1 << 2))
+                {
+                    arr[1 << 2] = !arr[1 << 2];
+                }
+                else if (i & (1 << 3))
+                {
+                    arr[1 << 3] = !arr[1 << 3];
+                }
+            }
         }
         i++;
     }
@@ -48,20 +52,20 @@ unsigned short getCodeFor(char ch)
     getPrePriorityChangeValue(ch);
     unsigned short result = 0;
 
-    for (unsigned char powOf2 = 1; powOf2 <= MESSAGE_SIZE; powOf2 = powOf2 << 1)
-    {
-        char parity = 0;
+    // for (unsigned char powOf2 = 1; powOf2 <= MESSAGE_SIZE; powOf2 = powOf2 << 1)
+    // {
+    //     char parity = 0;
 
-        for (int j = powOf2; j <= MESSAGE_SIZE; j = j + 2 * powOf2)
-            for (unsigned char k = j; k < j + powOf2 && k <= MESSAGE_SIZE; k++)
-            {
-                if (arr[k])
-                {
-                    parity = !parity;
-                }
-            }
-        arr[powOf2] = parity;
-    }
+    //     for (int j = powOf2; j <= MESSAGE_SIZE; j = j + 2 * powOf2)
+    //         for (unsigned char k = j; k < j + powOf2 && k <= MESSAGE_SIZE; k++)
+    //         {
+    //             if (arr[k])
+    //             {
+    //                 parity = !parity;
+    //             }
+    //         }
+    //     arr[powOf2] = parity;
+    // }
     for (int i = 1; i <= MESSAGE_SIZE; i++)
     {
         result |= arr[i] << (MESSAGE_SIZE - i);
