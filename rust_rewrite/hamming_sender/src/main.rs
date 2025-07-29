e452c2c78745186c8399e556235ad27dd47b6597
Soham Metha <sohammetha01@gmail.com>
2025-07-29 16:39:15 +0000

    29/07/2025 : 22:09:15

--- rust_rewrite/hamming_sender/src/main.rs
+++ rust_rewrite/hamming_sender/src/main.rs
@@ -19,9 +19,7 @@ fn main() {
 
     let mut in_f = fs::File::open(&args[1]).unwrap();
     let mut characters = String::new();
-    in_f.read_to_string(&mut characters);
-
-    let mut out: [HammingSymbol; 100];
+    in_f.read_to_string(&mut characters).unwrap();
 
     println!("\n================= SENDER =================\n");
     println!("\nIN : {}\nOUT: {}\n", args[1], args[2]);
@@ -29,14 +27,11 @@ fn main() {
 
     for (i, ch) in characters.chars().enumerate() {
         let ch = ch as u8;
-        out[i] = HammingSymbol {
+        let code = HammingSymbol {
             char_val: ch,
             encoded_val: 0,
-        }
-    }
-
-    for symbol in &out {
-        out_f.write_all(&symbol.encoded_val.to_be_bytes()).unwrap();
+        };
+        out_f.write_all(&code.encoded_val.to_be_bytes()).unwrap();
     }
 
     println!("\n┌─────┬──────────────┐\n");
