ec20950f3c974da4034467144b90898fa7548ec3
Soham Metha <sohammetha01@gmail.com>
2025-07-29 19:05:26 +0000

    30/07/2025 : 00:35:26

--- src/hamming/receiver/receiver.c
+++ src/hamming/receiver/receiver.c
@@ -5,19 +5,19 @@
 int main(int argc, char **argv)
 {
     assert(getFiles(&argc, &argv));
-    int8 *encodedText = (int8 *)(&in[1]);
-
     printf("\n================ RECEIVER ================\n"
-           "\nIN  : %s\nOUT : %s\n"
-           "\n┌───────────────────┬─────┬──────────────┐",
+           "\nIN  : %s\nOUT : %s\n",
            argv[1], argv[2]);
 
+    int8 *encodedText = (int8 *)(&in[1]);
+
+    printf("\n┌───────────────────┬─────┬──────────────┐");
     for (unsigned char i = 0; i < in[0]; i++)
     {
         printf("\n│  ");
         char ch = getDecodedCharFrom(encodedText[i]);
 
-        printf("  │  %c  │ ", char_val(ch));
+        printf("  │  %c  │ ", (ch == '\n') ? '\\' : ch);
         PRINT_BITS(MESSAGE_SIZE - 1, 0, encodedText[i])
         printf(" │");
 
