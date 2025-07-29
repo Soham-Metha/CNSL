fcecaf1b04467f2ef3d0cc7edd805efcc4bfbfc0
Soham Metha <sohammetha01@gmail.com>
2025-07-29 18:38:39 +0000

    30/07/2025 : 00:08:39

--- src/common/fileOps.c
+++ src/common/fileOps.c
@@ -1,6 +1,18 @@
 #include <fileOps.h>
 #include <stdlib.h>
 
+typedef unsigned char i8;
+typedef unsigned short int i16;
+typedef unsigned int i32;
+typedef unsigned long long int i64;
+
+#define $1 (i8 *)
+#define $2 (i16)
+#define $4 (i32)
+#define $8 (i64)
+#define $c (char *)
+#define $i (int)
+
 FILE *inF, *outF;
 char *in;
 
