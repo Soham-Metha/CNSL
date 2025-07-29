51927e618e389ead3a19fbf61b78ea373890c281
Soham Metha <sohammetha01@gmail.com>
2025-07-29 19:05:38 +0000

    30/07/2025 : 00:35:38

--- src/hamming/receiver/receiver.c
+++ src/hamming/receiver/receiver.c
@@ -1,15 +1,19 @@
-#include <assert.h>
 #include <fileOps.h>
 #include <hamming.h>
 
 int main(int argc, char **argv)
 {
-    assert(getFiles(&argc, &argv));
     printf("\n================ RECEIVER ================\n"
            "\nIN  : %s\nOUT : %s\n",
            argv[1], argv[2]);
 
-    int8 *encodedText = (int8 *)(&in[1]);
+    if (!getFiles(&argc, &argv))
+    {
+        printf("INPUT FILE IS EMPTY");
+        return 1;
+    }
+
+    unsigned short *encodedText = (unsigned short *)(&in[1]);
 
     printf("\n┌───────────────────┬─────┬──────────────┐");
     for (unsigned char i = 0; i < in[0]; i++)
