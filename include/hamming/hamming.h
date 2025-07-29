15ddaffc48531618e05eabdcf334827fbf1ac1e9
Soham Metha <sohammetha01@gmail.com>
2025-07-29 18:55:04 +0000

    30/07/2025 : 00:25:04

--- include/hamming/hamming.h
+++ include/hamming/hamming.h
@@ -3,6 +3,7 @@
 #define DATA_BIT_CNT 8
 #define PARITY_COUNT 4
 #define MESSAGE_SIZE (DATA_BIT_CNT + PARITY_COUNT)
+#define char_val(v) (v == '\n') ? '\\' : v
 #define PRINT_BITS(ub, lb, data)                                                                                       \
     {                                                                                                                  \
         for (int __j = ub; __j >= lb; __j--)                                                                           \
