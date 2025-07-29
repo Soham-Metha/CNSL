1be48189d7dae12b5ae6c876423b1796b93d7dec
Soham Metha <sohammetha01@gmail.com>
2025-07-29 21:43:21 +0000

    30/07/2025 : 03:13:21

--- src/hamming/sender/hammingEncoder.c
+++ src/hamming/sender/hammingEncoder.c
@@ -10,25 +10,25 @@ unsigned short getCodeFor(char ch)
     for (char chLen = DATA_BIT_CNT - 1, powOf2 = 1; chLen >= 0; i++)
     {
         if (i == powOf2)
-        {
+        { // skip parity bits(present at location 'powerOf2')
             powOf2 = powOf2 << 1;
             continue;
         }
 
         if (ch & (1 << (chLen)))
-        {
+        { // Load value into arr, & update corresponding parity bits
             arr[i] = 1;
             for (char j = 0; j < PARITY_COUNT; j++)
                 // arr[1 << j] ^= !!(i & (1 << j));
                 if (i & (1 << j))
                     arr[(1 << j)] = !arr[(1 << j)];
         }
-
         chLen--;
     }
 
     unsigned short result = 0;
     for (int i = 1; i <= MESSAGE_SIZE; i++)
+        // Load all Bits into 'result'
         result |= arr[i] << (MESSAGE_SIZE - i);
 
     return result;
